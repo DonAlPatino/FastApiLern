3233c4c19de1b14e426e8d89d005dcf91c91a2ef
DonAlPatino <valp@inbox.ru>
2024-11-25 13:27:30 +0300

    add .env file in config.py

--- app/database.py
+++ app/database.py
@@ -1,18 +1,19 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
-# from app.config import get_db_url
+from app.config import get_db_url
 
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = 'fast_api'
-DB_USER = 'postgres'
-DB_PASSWORD = 'postgres'
+# .env file
+# DB_HOST = 'localhost'
+# DB_PORT = '5432'
+# DB_NAME = 'fast_api'
+# DB_USER = 'postgres'
+# DB_PASSWORD = 'postgres'
 
-DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+#DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 
-# DATABASE_URL = get_db_url()
+DATABASE_URL = get_db_url()
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
