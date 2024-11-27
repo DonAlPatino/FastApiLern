34d54aea4d474e14cc6c3fe35b6accd0b140c550
DonAlPatino <valp@inbox.ru>
2024-11-27 15:28:31 +0300

    add router and get gata methods

--- app/students/rb.py
+++ app/students/rb.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-
 class RBStudent:
     def __init__(self, student_id: int | None = None,
                  course: int | None = None,
@@ -12,8 +9,9 @@ class RBStudent:
         self.enrollment_year = enrollment_year
 
     def to_dict(self) -> dict:
-        data = {'id': self.id, 'course': self.course, 'major_id': self.major_id,
-                'enrollment_year': self.enrollment_year}
-        # Создаем копию словаря, чтобы избежать изменения словаря во время итерации
-        filtered_data = {key: value for key, value in data.items() if value is not None}
-        return filtered_data
+        return {key: value for key, value in {
+            'id': self.id,
+            'course': self.course,
+            'major_id': self.major_id,
+            'enrollment_year': self.enrollment_year
+        }.items() if value is not None}
--- app/students/router.py
+++ app/students/router.py
@@ -9,3 +9,21 @@ router = APIRouter(prefix='/students', tags=['Работа со студента
 @router.get("/", summary="Получить всех студентов")
 async def get_all_students(request_body: RBStudent = Depends()) -> list[SStudent]:
     return await StudentDAO.find_all(**request_body.to_dict())
+
+
+@router.get("/{id}", summary="Получить одного студента по id")
+async def get_student_by_id(student_id: int) -> SStudent | dict:
+    rez = await StudentDAO.find_one_or_none_by_id(student_id)
+    if rez is None:
+        return {'message': f'Студент с ID {student_id} не найден!'}
+    # return SStudent.model_validate(rez).model_dump()
+    return rez
+
+
+@router.get("/by_filter", summary="Получить одного студента по фильтру")
+async def get_student_by_filter(request_body: RBStudent = Depends()) -> SStudent | dict:
+    rez = await StudentDAO.find_one_or_none(**request_body.to_dict())
+    if rez is None:
+        return {'message': f'Студент с указанными вами параметрами не найден!'}
+    return rez
+
